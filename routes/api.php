3811296fccc381045df0f0fdf98634c6e8aaef56
Kevin Bastian <kjb5642@rit.edu>
2017-12-21 16:30:25 -0500

    Agregado mas rutas para gimnasios y ciudades en la API

--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,28 @@ Route::get('/gimnastas', function () {
     return App\Gimnasta::with(['Gimnasio.Ciudad','Nivel','Rango'])->get();
 });
 
-// Obtener todas las ciudades con gimnasios y gimnastas
-Route::get('/ciudades', function () {
-    return App\Ciudad::with('gimnasios','gimnastas')->get();
+// Obtener una gimnasta en especifico con toda la informacion
+Route::get('/gimnastas/{gimnasta}', function (App\Gimnasta $gimnasta) {
+    return $gimnasta->load('gimnasio','nivel', 'rango');
 });
 
+// Obtener todas los gimnasios con las gimnastas de cada uno
+Route::get('/gimnasios', function () {
+    return App\Gimnasio::with(['Gimnastas'])->get();
+});
+
+// Obtener un gimnasio con sus gimnastas
+Route::get('/gimnasios/{gimnasio}', function (App\Gimnasio $gimnasio) {
+    return $gimnasio->load('Gimnastas');
+});
+
+// Obtener todas las ciudades con gimnasios
+Route::get('/ciudades', function () {
+    return App\Ciudad::with('gimnasios')->get();
+});
+
+// Obtener una ciudad con sus gimnasios y gimnastas
+Route::get('/ciudades/{ciudad}', function (App\Ciudad $ciudad) {
+    return $ciudad->load('gimnasios','gimnastas');
+});
 
